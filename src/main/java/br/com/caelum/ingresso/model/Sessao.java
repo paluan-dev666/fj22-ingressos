500e4e5ab5a3d40584bf874e1f91a0b8d202143f
paluan <teste@teste.com>
2020-01-28 14:00:16 -0300

    adicionando sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,28 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
+	
+	public Sessao () {
+		
+	}
 	
 	public Integer getId() {
 		return id;
@@ -42,7 +58,6 @@ public class Sessao {
 	}
 
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
-		super();
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
